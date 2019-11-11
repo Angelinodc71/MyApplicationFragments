97e0b339903d0d999ac688f3df0a79a99b0a82ec
angelinodc71 <adec58@elpuig.xeill.nety>
2019-11-11 17:05:44 +0100

    Initial commit

---
+++ app/src/main/java/com/example/myapplicationfragments/HomeFragment.java
@@ -0,0 +1,61 @@
+package com.example.myapplicationfragments;
+
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class HomeFragment extends Fragment {
+
+
+    public HomeFragment() {}
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_home, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        Button buttonLogin = view.findViewById(R.id.btnLogin);
+        startActivity(new Intent(this, LoginFragment.class));
+        Navigation.findNavController(view).navigate(R.id.LoginFragment);
+    }
+
+    //    Button buttonLogin = findViewById(R.id.btnLogin);
+//    Button buttonRegister = (Button) findViewById(R.id.btnRegister);
+//
+//        buttonLogin.setOnClickListener(new View.OnClickListener() {
+//        @Override
+//        public void onClick(View view) {
+//            Intent intent = new Intent(MainActivity.this, Login.class);
+//            startActivity(intent);
+//        }
+//    });
+//        buttonRegister.setOnClickListener(new View.OnClickListener() {
+//        @Override
+//        public void onClick(View view) {
+//            Intent intent = new Intent(MainActivity.this, Register.class);
+//            startActivity(intent);
+//        }
+//    });
+
+}
