04dcfaea74680d485ac99f744870417b75eb83b1
Angelinodc71 <adec58@elpuig.xeill.net>
2019-12-11 14:57:46 +0100

    Vamoh avansando, Problemas en el LiveCycle

--- app/src/main/java/com/example/myapplicationfragments/VerNoticiaFragment.java
+++ app/src/main/java/com/example/myapplicationfragments/VerNoticiaFragment.java
@@ -3,29 +3,53 @@ package com.example.myapplicationfragments;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-
-import com.example.myapplicationfragments.R;
-
+import android.widget.TextView;
 
 
 public class VerNoticiaFragment extends Fragment {
 
+    PrincipalViewModel principalViewModel;
 
-    public VerNoticiaFragment() {
-        // Required empty public constructor
-    }
+    TextView tituloTextView, subtituloTextView, imagenTextView, categoriaTextView;
+
+    public VerNoticiaFragment() { }
 
 
     @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detalle_elemento, container, false);
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_ver_noticia, container, false);
     }
 
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+
+        principalViewModel = ViewModelProviders.of(requireActivity()).get(PrincipalViewModel.class);
+
+        tituloTextView = view.findViewById(R.id.textview_titulo);
+        subtituloTextView = view.findViewById(R.id.textview_subtitulo);
+        imagenTextView = view.findViewById(R.id.textview_imagen);
+        categoriaTextView = view.findViewById(R.id.textview_categoria);
+
+        principalViewModel.noticiaSeleccionada.observe(getViewLifecycleOwner(), new Observer<Noticia>() {
+            @Override
+            public void onChanged (final Noticia noticia) {
+                if (noticia == null) return;
+
+                tituloTextView.setText(noticia.titulo);
+                subtituloTextView.setText(noticia.subtitulo);
+                imagenTextView.setText(noticia.imagen);
+                categoriaTextView.setText(noticia.categoria);
+            }
+        });
+    }
 }
