a72690d3e76bbd9d68cb6aefa15fb83014d71686
Angelinodc71 <adec58@elpuig.xeill.net>
2020-01-21 03:10:27 +0100

    Español creado, Default Ingles, Vistas horizontales y dimensiones

--- app/src/main/java/com/example/myapplicationfragments/SwitchCategoriesFragment.java
+++ app/src/main/java/com/example/myapplicationfragments/SwitchCategoriesFragment.java
@@ -3,17 +3,11 @@ package com.example.myapplicationfragments;
 
 import android.os.Bundle;
 
-import androidx.annotation.NonNull;
-import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-
-import com.google.android.material.tabs.TabLayout;
 
 
 /**
@@ -31,7 +25,7 @@ public class SwitchCategoriesFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_category_one, container, false);
+        return inflater.inflate(R.layout.fragment_switch_categories, container, false);
     }
 
 //    @Override
